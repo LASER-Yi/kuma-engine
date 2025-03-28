a5f9855aa9a4e8c2c49485fbf4e6ba4282a680f0
LASER-Yi <liangyi0007@gmail.com>
2025-03-29 02:46:33 +0800

    Init project structure

---
+++ CMake/CompileOptions.cmake
@@ -0,0 +1,15 @@
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+
+    # Only do this on macOS, fix later
+    if (APPLE)
+        add_compile_options(-fsanitize=address)
+        add_link_options(-fsanitize=address)
+    endif()
+endif()
+
+# Warning as error
+if(MSVC)
+    add_compile_options(/W4 /WX)
+else()
+    add_compile_options(-Wall -Wextra -Wpedantic -Werror)
+endif()
---
+++ src/Engine/Private/KumaEngine.cpp
@@ -0,0 +1,1 @@
+#include "KumaEngine.h"
