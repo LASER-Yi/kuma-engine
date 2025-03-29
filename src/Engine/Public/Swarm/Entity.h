a080c4f98425ac9b2ce0b9b919920778950c287d
LASER-Yi <liangyi0007@gmail.com>
2025-03-29 15:33:40 +0800

    Implement AddComponent and RemoveComponent placeholders in FEntity

--- src/Engine/Public/Swarm/Entity.h
+++ src/Engine/Public/Swarm/Entity.h
@@ -26,6 +26,36 @@ struct FEntity {
         assert(true && "GetComponent not implemented");
     }
 
+    template<typename T> T* AddComponent()
+    {
+        static_assert(std::is_base_of<IComponent, T>::value,
+                      "T must be derived from ISwarmComponent");
+
+        const Swarm::ComponentType ComponentIndex = Components[T::GetType()];
+
+        if (ComponentIndex != Swarm::InvalidIndex) {
+            assert(true && "AddComponent not implemented");
+        }
+
+        // Add the component to the component manager
+        assert(true && "AddComponent not implemented");
+    }
+
+    template<typename T> void RemoveComponent()
+    {
+        static_assert(std::is_base_of<IComponent, T>::value,
+                      "T must be derived from ISwarmComponent");
+
+        const Swarm::ComponentType ComponentIndex = Components[T::GetType()];
+
+        if (ComponentIndex == Swarm::InvalidIndex) {
+            return;
+        }
+
+        // Remove the component from the component manager
+        assert(true && "RemoveComponent not implemented");
+    }
+
   private:
     std::array<Swarm::ComponentIndexType, Swarm::MaxComponents> Components;
 };
