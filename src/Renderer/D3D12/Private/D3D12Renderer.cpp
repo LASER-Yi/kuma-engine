498baffdb14c5062e7d3f2d5c3a7932885269d86
Liang Yi <liangyi2566@virtuos.com.cn>
2025-04-03 17:37:56 +0800

    Add basic D3D12 usage

--- src/Renderer/D3D12/Private/D3D12Renderer.cpp
+++ src/Renderer/D3D12/Private/D3D12Renderer.cpp
@@ -3,17 +3,17 @@
 #include <d3d12.h>
 #include <wrl/client.h>
 
-
 void KD3D12Renderer::Initialize(void* WindowPtr)
 {
+#ifdef KUMA_BUILD_DEBUG
     {
-        // TODO: Conditional DebugController
         Microsoft::WRL::ComPtr<ID3D12Debug> DebugController;
         if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController))))
-		{
+        {
             DebugController->EnableDebugLayer();
-		}
+        }
     }
+#endif
 }
 
 void KD3D12Renderer::Update() {}
