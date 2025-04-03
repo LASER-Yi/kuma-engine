7019b4ff39063f05da135bdd1562033a50cf987f
Liang Yi <liangyi2566@virtuos.com.cn>
2025-04-03 17:33:28 +0800

    Link D3D12 library on WIN32

--- src/Renderer/D3D12/Private/D3D12Renderer.cpp
+++ src/Renderer/D3D12/Private/D3D12Renderer.cpp
@@ -1,6 +1,20 @@
 #include "D3D12Renderer.h"
 
-void KD3D12Renderer::Initialize(void* WindowPtr) {}
+#include <d3d12.h>
+#include <wrl/client.h>
+
+
+void KD3D12Renderer::Initialize(void* WindowPtr)
+{
+    {
+        // TODO: Conditional DebugController
+        Microsoft::WRL::ComPtr<ID3D12Debug> DebugController;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&DebugController))))
+		{
+            DebugController->EnableDebugLayer();
+		}
+    }
+}
 
 void KD3D12Renderer::Update() {}
 
