dfd60409db777231fa38b621148e304fdb215bb6
LASER-Yi <liangyi0007@gmail.com>
2025-04-09 19:10:58 +0800

    Implement render target creation

--- src/Renderer/D3D12/Private/D3D12Viewport.cpp
+++ src/Renderer/D3D12/Private/D3D12Viewport.cpp
@@ -4,7 +4,6 @@
 #include "D3D12Device.h"
 
 #include <cassert>
-#include <dxgi1_4.h>
 
 FD3D12Viewport::FD3D12Viewport(
     std::shared_ptr<FD3D12Device> Device, void* InWindowHandle
@@ -21,25 +20,65 @@ FD3D12Viewport::FD3D12Viewport(
         Height = WindowRect.bottom - WindowRect.top;
     }
 
-    IDXGIFactory* Factory = Device->GetFactory();
+    {
+        IDXGIFactory* Factory = Device->GetFactory();
 
-    DXGI_SWAP_CHAIN_DESC SwapChainDesc;
-    ZeroMemory(&SwapChainDesc, sizeof(SwapChainDesc));
+        DXGI_SWAP_CHAIN_DESC SwapChainDesc;
+        ZeroMemory(&SwapChainDesc, sizeof(SwapChainDesc));
 
-    SwapChainDesc.BufferCount = FrameCount;
-    SwapChainDesc.BufferDesc.Width = Width;
-    SwapChainDesc.BufferDesc.Height = Height;
-    SwapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    SwapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-    SwapChainDesc.OutputWindow = WindowHandle;
-    SwapChainDesc.SampleDesc.Count = 1;
-    SwapChainDesc.Windowed = TRUE;
+        SwapChainDesc.BufferCount = FrameCount;
+        SwapChainDesc.BufferDesc.Width = Width;
+        SwapChainDesc.BufferDesc.Height = Height;
+        SwapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        SwapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+        SwapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+        SwapChainDesc.OutputWindow = WindowHandle;
+        SwapChainDesc.SampleDesc.Count = 1;
+        SwapChainDesc.Windowed = TRUE;
 
-    const HRESULT Result = Factory->CreateSwapChain(
-        Device->CmdQueue->GetCommandQueue(), &SwapChainDesc, &SwapChain
-    );
-    assert(SUCCEEDED(Result));
+        Microsoft::WRL::ComPtr<IDXGISwapChain> NewSwapChain;
+        const HRESULT Result = Factory->CreateSwapChain(
+            Device->CmdQueue->GetCommandQueue(), &SwapChainDesc, &NewSwapChain
+        );
+        assert(SUCCEEDED(Result));
+
+        const bool AssignResult = SUCCEEDED(NewSwapChain.As(&SwapChain));
+        assert(AssignResult);
+    }
+
+    constexpr DXGI_RGBA Background = {0.0, 0.0, 0.0, 1.0};
+    SwapChain->SetBackgroundColor(&Background);
+    FrameIndex = SwapChain->GetCurrentBackBufferIndex();
+
+    ID3D12Device* DevicePtr = Device->GetDevice();
+    {
+        D3D12_DESCRIPTOR_HEAP_DESC RenderTargetHeapDesc;
+        ZeroMemory(&RenderTargetHeapDesc, sizeof(D3D12_DESCRIPTOR_HEAP_DESC));
+
+        RenderTargetHeapDesc.NumDescriptors = FrameCount;
+        RenderTargetHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        RenderTargetHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+        const bool Result = SUCCEEDED(DevicePtr->CreateDescriptorHeap(
+            &RenderTargetHeapDesc, IID_PPV_ARGS(&RenderTargetHeap)
+        ));
+        assert(Result);
+
+        std::size_t RenderTargetDescriptorSize =
+            DevicePtr->GetDescriptorHandleIncrementSize(
+                D3D12_DESCRIPTOR_HEAP_TYPE_RTV
+            );
+
+        D3D12_CPU_DESCRIPTOR_HANDLE RenderTargetHandle = RenderTargetHeap->GetCPUDescriptorHandleForHeapStart();
+
+        for (std::uint32_t i = 0; i < FrameCount; ++i)
+        {
+            SwapChain->GetBuffer(i, IID_PPV_ARGS(&RenderTargets[i]));
+            DevicePtr->CreateRenderTargetView(RenderTargets[i].Get(), nullptr, RenderTargetHandle);
+
+            RenderTargetHandle.ptr += RenderTargetDescriptorSize;
+        }
+    }
 
     Device->Viewport = this;
 }
--- src/Renderer/D3D12/Private/D3D12Viewport.h
+++ src/Renderer/D3D12/Private/D3D12Viewport.h
@@ -1,9 +1,10 @@
 ﻿#pragma once
 
 #include <memory>
+#include <array>
 
 #include <d3d12.h>
-#include <dxgi.h>
+#include <dxgi1_5.h>
 #include <wrl/client.h>
 
 struct FD3D12Device;
@@ -12,11 +13,18 @@ struct FD3D12Viewport
 {
     FD3D12Viewport(std::shared_ptr<FD3D12Device> Device, void* InWindowHandle);
 
+    static constexpr std::uint32_t MaxFrameCount = 3;
+
 private:
-    Microsoft::WRL::ComPtr<IDXGISwapChain> SwapChain;
+    Microsoft::WRL::ComPtr<IDXGISwapChain4> SwapChain;
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> RenderTargetHeap;
+
+    std::array<Microsoft::WRL::ComPtr<ID3D12Resource>, MaxFrameCount> RenderTargets;
 
     HWND WindowHandle;
     std::uint32_t Width;
     std::uint32_t Height;
     std::uint32_t FrameCount;
+
+    std::uint32_t FrameIndex;
 };
