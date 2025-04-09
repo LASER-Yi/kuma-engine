fc8d9f7310a24837ed5f2cd2d0ead98746d51659
LASER-Yi <liangyi0007@gmail.com>
2025-04-09 20:17:18 +0800

    Update Plane implementation

--- src/Math/Public/Plane.h
+++ src/Math/Public/Plane.h
@@ -1,21 +1,20 @@
 #pragma once
 
+#include "Vector.h"
+
 #include <type_traits>
 
 namespace Math
 {
 
 template <typename T>
-struct alignas(16) TPlane
+struct alignas(16) TPlane : TVector<T>
 {
     static_assert(std::is_floating_point_v<T>, "T must be floating point");
 
     TPlane() = default;
-    TPlane(T InX, T InY, T InZ, T InW) : X(InX), Y(InY), Z(InZ), W(InW) {}
+    TPlane(T InX, T InY, T InZ, T InW) : TVector<T>(InX, InY, InZ), W(InW) {}
 
-    T X;
-    T Y;
-    T Z;
     T W;
 };
 
